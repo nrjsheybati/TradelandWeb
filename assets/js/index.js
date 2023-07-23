d0371c4b01b7c936696c792c1e7753df6cdab0a4
amir z <aafz1382@gmail.com>
2023-07-23 09:22:23 +0330

    amirali

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,132 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        "0": {
-            slidesPerView: 1,
-        },
-        "768": {
-            slidesPerView: 2.5,
-        },
-        "992": {
-            slidesPerView: 3,
-        },
-        "1200": {
-            slidesPerView: 3,
-        },
-        "1400": {
-            slidesPerView: 3,
-        }
-    },
-});
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 10,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//         "0": {
+//             slidesPerView: 1,
+//         },
+//         "768": {
+//             slidesPerView: 2.5,
+//         },
+//         "992": {
+//             slidesPerView: 3,
+//         },
+//         "1200": {
+//             slidesPerView: 3,
+//         },
+//         "1400": {
+//             slidesPerView: 3,
+//         }
+//     },
+// });
 
 
-//for eye icon 
-const togglePassword = document.getElementById('toggle-pass')
-let iTag = document.getElementById("iTag")
+// //for eye icon 
+// const togglePassword = document.getElementById('toggle-pass')
+// let iTag = document.getElementById("iTag")
 
-iTag.addEventListener('click', function () {
+// iTag.addEventListener('click', function () {
 
-    const type = togglePassword.getAttribute('type') == 'password' ? 'text' : 'password';
+//     const type = togglePassword.getAttribute('type') == 'password' ? 'text' : 'password';
 
-    togglePassword.setAttribute('type', type);
+//     togglePassword.setAttribute('type', type);
 
-    if (iTag.classList.contains('bi-eye')) {
-        iTag.classList.replace('bi-eye', 'bi-eye-slash');
-    }
-    else {
-        iTag.classList.replace('bi-eye-slash', 'bi-eye');
-    }
-});
-// for validations
+//     if (iTag.classList.contains('bi-eye')) {
+//         iTag.classList.replace('bi-eye', 'bi-eye-slash');
+//     }
+//     else {
+//         iTag.classList.replace('bi-eye-slash', 'bi-eye');
+//     }
+// });
+// // for validations
 
-(() => {
-    'use strict'
-    const forms = document.querySelectorAll('.needs-validation')
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
+// (() => {
+//     'use strict'
+//     const forms = document.querySelectorAll('.needs-validation')
+//     Array.from(forms).forEach(form => {
+//         form.addEventListener('submit', event => {
+//             if (!form.checkValidity()) {
+//                 event.preventDefault()
+//                 event.stopPropagation()
+//             }
 
-            form.classList.add('was-validated') // add this line
-        }, false)
-    })
-})()
+//             form.classList.add('was-validated') // add this line
+//         }, false)
+//     })
+// })()
 
 //numeric-inputs
 
-function onlyNumberKey(evt) {
-             
-    // Only ASCII character in that range allowed
-    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
-    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
-        return false;
-    return true;
-}
+// function onlyNumberKey(evt) {
 
-//changing the text under the input if it is empty
+//     // Only ASCII character in that range allowed
+//     var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+//     if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+//         return false;
+//     return true;
+// }
 
+//high chart
+
+// create the data array
+
+
+// const high = new stockChart()
+
+
+
+(async () => {
+
+    const data = await fetch(
+        'https://demo-live-data.highcharts.com/aapl-ohlc.json'
+    ).then(response => response.json());
+
+
+    // create the chart
+    const chart = Highcharts.stockChart('container', {
+        rangeSelector: {
+            selected: 1,
+            enabled: false,
+        },
+
+        navigator: {
+            enabled: false
+        },
+        scrollbars: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            }
+        },
+
+        xAxis: {
+            accessibility: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            labels: {
+                enabled: false
+            }
+        },
+        series: [{
+            type: 'candlestick',
+            name: 'AAPL Stock Price',
+            data: data
+        }],
+        exporting: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+
+
+    });
+})();
